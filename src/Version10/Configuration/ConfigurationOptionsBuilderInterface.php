09cefd49e43514f71eefff08387816d5343de0b6
David Molineus <david.molineus@netzmacht.de>
2020-06-15 22:25:15 +0200

    Add supportDirectories() flag

--- src/Version10/Configuration/ConfigurationOptionsBuilderInterface.php
+++ src/Version10/Configuration/ConfigurationOptionsBuilderInterface.php
@@ -12,6 +12,17 @@ use Phpcq\PluginApi\Version10\Configuration\Builder\OptionBuilderInterface;
 interface ConfigurationOptionsBuilderInterface
 {
     /**
+     * Enable support for directory based configurations.
+     *
+     * If enabled it's possible to provide directory specific configurations. Furthermore plugins expecting the
+     * \Phpcq\PluginApi\Version10\Configuration\PluginConfigurationInterface will get an list option directories for
+     * which the passed configuration applies to.
+     *
+     * @return $this
+     */
+    public function supportDirectories(): self;
+
+    /**
      * Create a sub array configuration and return its builder.
      *
      * @param string $name        The name of the option.
