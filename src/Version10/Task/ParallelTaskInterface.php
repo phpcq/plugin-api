aeaab2d941e583bbb9801407babac07c548fee0d
Christian Schiffler <c.schiffler@cyberspectrum.de>
2020-06-18 04:41:42 +0200

    Add interfaces for parallel task execution

---
+++ src/Version10/Task/ParallelTaskInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpcq\PluginApi\Version10\Task;
+
+use Phpcq\PluginApi\Version10\RuntimeException;
+
+/**
+ * This interface describes a task that can be run in parallel to other tasks.
+ *
+ * Note that this base interface only handles the checking for completion and not the method how to start the task.
+ * This is done in concrete interfaces.
+ *
+ * A task will be triggered via the ParallelTaskInterface::tick() method as long as the method returns true.
+ *
+ * To signal abnormal execution, a task may throw a RuntimeException from within the tick method only.
+ */
+interface ParallelTaskInterface extends TaskInterface
+{
+    /**
+     * Called repeatedly by the scheduler to give a time slice to the task.
+     *
+     * The result is false if the task has finished or true if the task shall get notified in the next loop again.
+     *
+     * @return bool
+     *
+     * @throws RuntimeException When the task fails.
+     */
+    public function tick(): bool;
+}
---
+++ src/Version10/Task/ReportWritingParallelTaskInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpcq\PluginApi\Version10\Task;
+
+use Phpcq\PluginApi\Version10\RuntimeException;
+use Phpcq\PluginApi\Version10\ToolReportInterface;
+
+/**
+ * This interface describes a task that can be run in parallel to other tasks.
+ */
+interface ReportWritingParallelTaskInterface extends ReportWritingTaskInterface, ParallelTaskInterface
+{
+    /**
+     * Run task with a report.
+     *
+     * In contradiction to the parent interface, the method MUST return immediately and
+     * only perform long work within the tick method.
+     *
+     * @param ToolReportInterface $report The tool report being created.
+     *
+     * @throws RuntimeException When task initialization fails.
+     */
+    public function runWithReport(ToolReportInterface $report): void;
+}
