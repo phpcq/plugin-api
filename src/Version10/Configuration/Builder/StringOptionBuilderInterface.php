b2a3b7a79ae3e5d6c7acd946ccdcf6eac89df698
David Molineus <david.molineus@netzmacht.de>
2020-06-23 05:26:15 +0200

    Use OptionBuilderInterface as marker and move methods to concrete interfaces
    Accept code duplication for better return type detection. Accept code duplication in favour of DX

--- src/Version10/Configuration/Builder/StringOptionBuilderInterface.php
+++ src/Version10/Configuration/Builder/StringOptionBuilderInterface.php
@@ -10,6 +10,30 @@ namespace Phpcq\PluginApi\Version10\Configuration\Builder;
 interface StringOptionBuilderInterface extends OptionBuilderInterface
 {
     /**
+     * Mark option as required.
+     */
+    public function isRequired(): self;
+
+    /**
+     * Normalize a value for the required format. The normalizer is called before the validator.
+     *
+     * @param callable $normalizer
+     * @psalm-param callable(mixed): mixed $normalizer
+     */
+    public function withNormalizer(callable $normalizer): self;
+
+    /**
+     * Register a validator which validates the given value.
+     *
+     * A validator has to throw a \Phpcq\PluginApi\Version10\Exception\InvalidConfigurationException if an invalid value
+     * is given.
+     *
+     * @param callable $validator
+     * @psalm-param callable(mixed): void $validator
+     */
+    public function withValidator(callable $validator): self;
+
+    /**
      * Define a default value.
      *
      * The default value is used then no value is configured.
