c3060bb6b7112752a82308be23f716867b6cd208
David Molineus <david.molineus@netzmacht.de>
2020-09-24 16:00:23 +0200

    Fix param name

--- src/Version10/Report/ReportInterface.php
+++ src/Version10/Report/ReportInterface.php
@@ -11,5 +11,5 @@ interface ReportInterface
     public const STATUS_FAILED  = 'failed';
 
     /** @psalm-param array<string,string> $metadata */
-    public function addTaskReport(string $toolName, array $metadata = []): TaskReportInterface;
+    public function addTaskReport(string $taskName, array $metadata = []): TaskReportInterface;
 }
